912a6b9efcdc18815bc6cb6086e5dc22ce9f0a9f
SUMYAK JAIN <3456gaurav@gmail.com>
2020-07-20 23:59:30 +0530

    Update j.py

--- Classification/K-Nearest Neighbors/j.py
+++ Classification/K-Nearest Neighbors/j.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+
+
 df = pd.read_csv('https://raw.githubusercontent.com/sumyak/ML-Algos-and-Techniques/master/Classification/K-Nearest%20Neighbors/teleCust1000t.csv')
 X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
 X[0:5]
@@ -17,10 +19,13 @@ y = df['custcat'].values
 y[0:5]
 X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
 X[0:5]
+
 #data split
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
 print ('Train set:', X_train.shape,  y_train.shape)
 print ('Test set:', X_test.shape,  y_test.shape)
+
+
 k = 4
 #Train Model and Predict  
 neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
@@ -28,6 +33,8 @@ yhat = neigh.predict(X_test)
 print(yhat[0:5])
 print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
 print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+#k evaluation
 Ks = 10
 mean_acc = np.zeros((Ks-1))
 std_acc = np.zeros((Ks-1))
@@ -43,6 +50,8 @@ for n in range(1,Ks):
     std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
 
 print(mean_acc)
+
+
 #plot diffrent k
 plt.plot(range(1,Ks),mean_acc,'g')
 plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
